e9bf5c32b82d59842234ca64e2e910f34b978ecc
StenKuusk <sten.kuusk@voco.ee>
2024-05-22 12:50:10 +0300

    bug fix :I

--- app.js
+++ app.js
@@ -2,22 +2,20 @@ const express = require('express');
 const todoRoutes = require('./routes/todo.routes');
 const app = express();
 const mongodb = require('./mongodb/mongodb.connect');
+
 mongodb.connect();
 
 app.use(express.json())
 
+app.use('/todos', todoRoutes);
+
 app.use((error, req, res, next) => {
+    console.log(error)
     res.status(500).json({ message: error.message });
 });
-
-app.use('/todos', todoRoutes);
 
 app.get('/', (req, res) => {
     res.send('express test')
 });
-
-// app.listen(3015, () => {
-//     console.log('server is running')
-// });
 
 module.exports = app
